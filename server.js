3f36c6306231535208bf0d30c72732885fdc77f5
Astha <yastha.as@gmail.com>
2018-11-30 17:34:50 +0545

    Dbconection temporary changed to global and User Policy Crud Added

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const app = module.exports = express();
-const route= express.Router();
+const app = express();
+// const route = module.exports= express.Router();
     
  const parser = require('body-parser');
-
 
 // winstonHelper = require('./lib/helpers/log.helper');
 // var winston = require('winston');
@@ -15,6 +14,9 @@ const dbConnector = require('./lib/helpers/db.helper'),
     redisConnector = require('./lib/helpers/redis.helper');
     require('dotenv').config(`${__dirname}/.env`);
 
+    app.use(parser.urlencoded({ extended: false}));
+    app.use(parser.json());
+
      dbConnector.init(app);
      redisConnector.init(app);
      routeHelper.init(app);
@@ -23,27 +25,27 @@ const dbConnector = require('./lib/helpers/db.helper'),
 
     //  winstonHelper.init(app);
 //console.log('app => ', app.locals.db);
-app.get('/', (req, res, next) => {
-    console.log('Server is working fine');
-    next();
-}, function(req, res, next){
-  //  console.log( 'req.dbCon', req.dbCon )
-    res.send("Hello Again")
-} )
+// app.get('/', (req, res, next) => {
+//     console.log('Server is working fine');
+//     next();
+// }, function(req, res, next){
+//   //  console.log( 'req.dbCon', req.dbCon )
+//     res.send("Hello Again")
+// } )
 
-app.use(parser.json());
-app.use(parser.urlencoded({ extended: true}));
+
 //Morgan and Winston
    
 
 //database connection middleware
-app.use(function(req,res, next){
-    req.dbCon = app.locals.db;
-    next();
-})
-
-//app.use('/', route);
-//app.listen(8000);
+// app.use(function(req, res, next){
+//     console.log('Here-----------------');
+//     if (app.locals.db) {
+//         console.log('Here===================');
+//         req.dbCon = app.locals.db;
+//     }
+//     next();
+// })
 
 app.listen(8000,() => {
     console.log('Listening to the port on 8000');
