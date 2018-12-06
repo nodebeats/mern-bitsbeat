1f102a44f9456f762361f792ad7c77ae7de3b11f
Salina <salina.sallu@outlook.com>
2018-12-06 11:34:11 +0545

    express-validation is done

--- server.js
+++ server.js
@@ -1,29 +1,50 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 // const route = module.exports= express.Router();
-    
- const parser = require('body-parser');
+
+const parser = require("body-parser");
 
 // winstonHelper = require('./lib/helpers/log.helper');
 // var winston = require('winston');
-// require('winston-loggly-bulk'); 
+// require('winston-loggly-bulk');
 
-const dbConnector = require('./lib/helpers/db.helper'),
-    routeHelper = require('./lib/routes/index'),
-    logWriter = require('./lib/helpers/logwriter.helper'),
-    redisConnector = require('./lib/helpers/redis.helper');
-    require('dotenv').config(`${__dirname}/.env`);
+const dbConnector = require("./lib/helpers/db.helper"),
+  routeHelper = require("./lib/routes/index"),
+  logWriter = require("./lib/helpers/logwriter.helper"),
+  expressValidator = require("express-validator");
+redisConnector = require("./lib/helpers/redis.helper");
+require("dotenv").config(`${__dirname}/.env`);
 
-    app.use(parser.urlencoded({ extended: false}));
-    app.use(parser.json());
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
 
-     dbConnector.init(app);
-     redisConnector.init(app);
-     routeHelper.init(app);
-    logWriter.init(app);
-    //redisCache.init(app);
+dbConnector.init(app);
+redisConnector.init(app);
 
-    //  winstonHelper.init(app);
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
+
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+  })
+);
+
+routeHelper.init(app);
+logWriter.init(app);
+//redisCache.init(app);
+
+//  winstonHelper.init(app);
 //console.log('app => ', app.locals.db);
 // app.get('/', (req, res, next) => {
 //     console.log('Server is working fine');
@@ -33,9 +54,7 @@ const dbConnector = require('./lib/helpers/db.helper'),
 //     res.send("Hello Again")
 // } )
 
-
 //Morgan and Winston
-   
 
 //database connection middleware
 // app.use(function(req, res, next){
@@ -47,6 +66,6 @@ const dbConnector = require('./lib/helpers/db.helper'),
 //     next();
 // })
 
-app.listen(8000,() => {
-    console.log('Listening to the port on 8000');
-})
+app.listen(8000, () => {
+  console.log("Listening to the port on 8000");
+});
