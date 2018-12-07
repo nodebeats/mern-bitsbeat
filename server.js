aa1b7422cba7733bd94c071adfc0883b4c813f81
Astha <yastha.as@gmail.com>
2018-12-07 17:04:52 +0545

    Bug fix with status code and proper validation response

--- server.js
+++ server.js
@@ -1,67 +1,54 @@
 const express = require('express');
 const app = express(),
-expressValidator = require("express-validator");
+  expressValidator = require("express-validator");
 // const route = module.exports= express.Router();
-    
- const parser = require('body-parser');
+
+const parser = require('body-parser');
 
 const dbConnector = require('./lib/helpers/db.helper'),
-    routeHelper = require('./lib/routes/index'),
-    logWriter = require('./lib/helpers/logwriter.helper'),
-    redisConnector = require('./lib/helpers/redis.helper');
-    require('dotenv').config(`${__dirname}/.env`);
+  routeHelper = require('./lib/routes/index'),
+  logWriter = require('./lib/helpers/logwriter.helper'),
+  redisConnector = require('./lib/helpers/redis.helper');
+require('dotenv').config(`${__dirname}/.env`);
 
-    app.use(parser.urlencoded({ extended: false}));
-    app.use(parser.json());
-    dbConnector.init(app);
-    redisConnector.init(app);
+app.use(parser.urlencoded({
+  extended: false
+}));
+app.use(parser.json());
+dbConnector.init(app);
+redisConnector.init(app);
 
-    app.use(
-        expressValidator({errorFormatter: function(param, msg, value) {
-            var namespace = param.split("."),
-              root = namespace.shift(),
-              formParam = root;
-      
-            while (namespace.length) {
-              formParam += "[" + namespace.shift() + "]";
-            }
-            return {
-              param: formParam,
-              msg: msg,
-              value: value
-            };
-          }
-        })
-      );
-     
-     routeHelper.init(app);
-    logWriter.init(app);
-    //redisCache.init(app);
+app.use(
+  expressValidator({
+    errorFormatter: function (param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
 
-    //  winstonHelper.init(app);
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+  })
+);
+
+routeHelper.init(app);
+logWriter.init(app);
+//redisCache.init(app);
 //console.log('app => ', app.locals.db);
- app.get('/', (req, res, next) => {
-    console.log('Server is working fine');
-    next();
-}, function(req, res, next){
+app.get('/', (req, res, next) => {
+  console.log('Server is working fine');
+  next();
+}, function (req, res, next) {
   //  console.log( 'req.dbCon', req.dbCon )
-    res.send("Hello Again")
-} )
+  res.send("Hello Again")
+})
 
-
-//Morgan and Winston
-   
-
-//database connection middleware
-// app.use(function(req, res, next){
-//     console.log('Here-----------------');
-//     if (app.locals.db) {
-//         console.log('Here===================');
-//         req.dbCon = app.locals.db;
-//     }
-//     next();
-// })
-
-app.listen(8000,() => {
-    console.log('Listening to the port on 8000');
+app.listen(8000, () => {
+  console.log('Listening to the port on 8000');
 })
