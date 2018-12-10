8589c33a9e273a652bbc3e3d816d2a23811ffdf4
SharthakSP <sity1c216034@islingtoncollege.edu.np>
2018-12-10 11:50:38 +0545

    Pull request forced to make some changes.

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 const express = require("express");
 const app = express();
-=======
-const express = require('express');
-const app = express(),
-expressValidator = require("express-validator");
->>>>>>> authorization
 // const route = module.exports= express.Router();
 
 const parser = require("body-parser");
@@ -13,7 +7,6 @@ const parser = require("body-parser");
 const dbConnector = require('./lib/helpers/db.helper'),
     routeHelper = require('./lib/routes/index'),
     logWriter = require('./lib/helpers/logwriter.helper'),
-<<<<<<< HEAD
     expressValidator = require('express-validator');
 redisConnector = require('./lib/helpers/redis.helper');
 require('dotenv').config(`${__dirname}/.env`);
@@ -44,37 +37,6 @@ app.use(expressValidator({
 routeHelper.init(app);
 logWriter.init(app);
 //redisCache.init(app);
-=======
-    redisConnector = require('./lib/helpers/redis.helper');
-    require('dotenv').config(`${__dirname}/.env`);
-
-    app.use(parser.urlencoded({ extended: false}));
-    app.use(parser.json());
-    dbConnector.init(app);
-    redisConnector.init(app);
-
-    app.use(
-        expressValidator({errorFormatter: function(param, msg, value) {
-            var namespace = param.split("."),
-              root = namespace.shift(),
-              formParam = root;
-      
-            while (namespace.length) {
-              formParam += "[" + namespace.shift() + "]";
-            }
-            return {
-              param: formParam,
-              msg: msg,
-              value: value
-            };
-          }
-        })
-      );
-     
-     routeHelper.init(app);
-    logWriter.init(app);
-    //redisCache.init(app);
->>>>>>> f564c9f0fd3e6b49188e1566529a9e6f4f000ded
 
 //  winstonHelper.init(app);
 //console.log('app => ', app.locals.db);
