a166268ef313f0c5415757b94b90b20123dabc34
RajShresthas <rajpsdshrest@gmail.com>
2018-12-10 12:51:24 +0545

    Merge resolved solved

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-const express = require("express");
-const app = express();
-=======
 const express = require('express');
 const app = express(),
-expressValidator = require("express-validator");
->>>>>>> authorization
+    expressValidator = require("express-validator");
 // const route = module.exports= express.Router();
 
 const parser = require("body-parser");
@@ -13,78 +8,52 @@ const parser = require("body-parser");
 const dbConnector = require('./lib/helpers/db.helper'),
     routeHelper = require('./lib/routes/index'),
     logWriter = require('./lib/helpers/logwriter.helper'),
-<<<<<<< HEAD
-    expressValidator = require('express-validator');
-redisConnector = require('./lib/helpers/redis.helper');
+    redisConnector = require('./lib/helpers/redis.helper');
 require('dotenv').config(`${__dirname}/.env`);
 
 app.use(parser.urlencoded({ extended: false }));
 app.use(parser.json());
-
 dbConnector.init(app);
 redisConnector.init(app);
 
-app.use(expressValidator({
-    errorFormatter: function (param, msg, value) {
-        var namespace = param.split('.'),
-            root = namespace.shift(),
-            formParam = root;
-        while (namespace.length) {
-            formParam += '[' + namespace.shift() + ']';
-        }
-        return {
-            param: formParam,
-            msg: msg,
-            value: value
-        };
-    }
-}));
+app.use(
+    expressValidator({
+        errorFormatter: function (param, msg, value) {
+            var namespace = param.split("."),
+                root = namespace.shift(),
+                formParam = root;
 
+            while (namespace.length) {
+                formParam += "[" + namespace.shift() + "]";
+            }
+            return {
+                param: formParam,
+                msg: msg,
+                value: value
+            };
+        }
+    })
+);
 
 routeHelper.init(app);
 logWriter.init(app);
 //redisCache.init(app);
-=======
-    redisConnector = require('./lib/helpers/redis.helper');
-    require('dotenv').config(`${__dirname}/.env`);
-
-    app.use(parser.urlencoded({ extended: false}));
-    app.use(parser.json());
-    dbConnector.init(app);
-    redisConnector.init(app);
-
-    app.use(
-        expressValidator({errorFormatter: function(param, msg, value) {
-            var namespace = param.split("."),
-              root = namespace.shift(),
-              formParam = root;
-      
-            while (namespace.length) {
-              formParam += "[" + namespace.shift() + "]";
-            }
-            return {
-              param: formParam,
-              msg: msg,
-              value: value
-            };
-          }
-        })
-      );
-     
-     routeHelper.init(app);
-    logWriter.init(app);
-    //redisCache.init(app);
->>>>>>> f564c9f0fd3e6b49188e1566529a9e6f4f000ded
 
 //  winstonHelper.init(app);
 //console.log('app => ', app.locals.db);
- app.get('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
     console.log('Server is working fine');
     next();
-}, function(req, res, next){
-  //  console.log( 'req.dbCon', req.dbCon )
+}, function (req, res, next) {
+    //  console.log( 'req.dbCon', req.dbCon )
     res.send("Hello Again")
-} )
+})
+
+
+//Error middleware
+app.use(function (err, req, res, next) {
+    errorController.log_error(err, req, res, next);
+})
 
 //Morgan and Winston
 
@@ -99,5 +68,5 @@ logWriter.init(app);
 // })
 
 app.listen(8000, () => {
-  console.log("Listening to the port on 8000");
+    console.log("Listening to the port on 8000");
 });
