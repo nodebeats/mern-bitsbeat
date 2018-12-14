492ff2e830005dc70e1e87751021c118158a5b8e
Salina <salina.sallu@outlook.com>
2018-12-14 14:00:05 +0545

    Email verification

--- server.js
+++ server.js
@@ -70,68 +70,6 @@ app.use(function (err, req, res, next) {
 //     next();
 // })
 
-/*------------------SMTP Sarted-----------------------------*/
-var smtpTransport = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-        user: "Your Gmail ID",
-        pass: "Gmail Password"
-    }
-});
-var rand,mailOptions,host,link;
-/*------------------SMTP Over-----------------------------*/
-
-/*------------------Routing Started ------------------------*/
-
-app.get('/index',function(req,res){
-    res.sendfile('index.html');
-});
-app.get('/send',function(req,res){
-        rand=Math.floor((Math.random() * 100) + 54);
-    host=req.get('host');
-    link="http://"+req.get('host')+"/verify?id="+rand;
-    mailOptions={
-        to : req.query.to,
-        subject : "Please confirm your Email account",
-        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>" 
-    }
-    console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function(error, response){
-     if(error){
-            console.log(error);
-        res.end("error");
-     }else{
-            console.log("Message sent: " + response.message);
-        res.end("sent");
-         }
-});
-});
-
-app.get('/verify',function(req,res){
-console.log(req.protocol+":/"+req.get('host'));
-if((req.protocol+"://"+req.get('host'))==("http://"+host))
-{
-    console.log("Domain is matched. Information is from Authentic email");
-    if(req.query.id==rand)
-    {
-        console.log("email is verified");
-        res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
-    }
-    else
-    {
-        console.log("email is not verified");
-        res.end("<h1>Bad Request</h1>");
-    }
-}
-else
-{
-    res.end("<h1>Request is from unknown source</h1>");
-}
-});
-
-
-
-
 app.listen(8000, () => {
     console.log("Listening to the port on 8000");
 });
